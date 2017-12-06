c1181a4cd21c19f7d81ff219a8cdb56c6dc3e6e3
ning <ningyuan.sg@gmail.com>
2017-12-07 01:35:54 +0800

    Some minor cleaning

--- day2-corruptionchecksum.py
+++ day2-corruptionchecksum.py
@@ -2,20 +2,17 @@
 
 from given_inputs import day2
 
-def multilinestr_to_list_of_list_of_int(string):
-    ''' splits a multi-line string into a list of strings by newline delimiter,
-        then splits said string entries by tab delimiter '''
-    return [ list(map(int, row.split('\t'))) for row in string.split('\n') ]
-
-spreadsheet_list = multilinestr_to_list_of_list_of_int(day2)
-min_max_list = [ (min(row), max(row)) for row in spreadsheet_list ]
+spreadsheet_2dlist = [ 
+        list(map(int, row.split('\t'))) 
+            for row in day2.split('\n') ]
+min_max_list = [ (min(row), max(row)) for row in spreadsheet_2dlist ]
 checksum = sum([ min_max[1] - min_max[0] for min_max in min_max_list ])
 
-print(checksum) # 32020
+print(checksum)  # Part 1: 32020
 
 def is_evenly_divisible(int1, int2):
-    return int1 / int2 % 1 == 0 if int1 > int2 \
-            else int2 / int1 % 1 == 0
+    return int1 % int2 == 0 if int1 > int2 \
+            else int2 % int1 == 0
 
 def find_evenly_divisibles(row):
     for i in range(len(row)):
@@ -29,8 +26,8 @@ def evenly_divide(ints):
             else ints[1] // ints[0]
 
 result = 0
-for row in spreadsheet_list:
+for row in spreadsheet_2dlist:
     evenly_divisibles = find_evenly_divisibles(row)
     result += evenly_divide(evenly_divisibles)
 
-print(result)
+print(result)  # Part 2: 236
