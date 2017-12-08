63d53b7ca48d8232cb13b0a2d465b7b77db6aa2f
ning <ningyuan.sg@gmail.com>
2017-12-08 13:30:12 +0800

    Add 4a, 4b

---
+++ day4_highentropypassphrases.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+from given_inputs import day4
+from collections import Counter
+
+# list<list<str>>
+passphrases = [ row.split() for row in day4.split('\n') ]
+valids = 0
+for passphrase in passphrases:
+    if len(passphrase) == len(set(passphrase)):
+        valids += 1
+print(valids)  # Part 1: 337
+
+valids = 0
+for passphrase in passphrases:
+    previous_words = [Counter(passphrase[0])]  # init
+    for word in passphrase[1:]:
+        alphabets = Counter(word)
+        if alphabets in previous_words:
+            break
+        else:
+            previous_words.append(alphabets)
+    else:
+        # only if for loop does not break,
+        # i.e. no anagrams
+        valids += 1
+print(valids)  # Part 2: 231
