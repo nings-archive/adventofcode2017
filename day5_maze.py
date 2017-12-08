a2f11f9e6cea5710cf8074c4a82ea2a6664d2419
ning <ningyuan.sg@gmail.com>
2017-12-08 16:12:31 +0800

    Add 5a, 5b

---
+++ day5_maze.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+from given_inputs import day5
+
+jumps = [ int(offset) for offset in day5.split('\n') ]
+length = len(jumps)
+index = 0
+steps = 0
+
+while index < length:
+    jumps[index] += 1
+    index += jumps[index] - 1
+    steps += 1
+
+print(steps)  # Part 1: 343467
+
+jumps = [ int(offset) for offset in day5.split('\n') ]
+length = len(jumps)
+index = 0
+steps = 0
+
+while index < length:
+    last_position = index
+    index += jumps[last_position]
+    if jumps[last_position] >= 3:
+        jumps[last_position] -= 1
+    else:
+        jumps[last_position] += 1
+    steps += 1
+
+print(steps)  # Part 2: 24774780
