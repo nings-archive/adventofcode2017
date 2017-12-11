e59e069fc7d2ab7da6c6b3393ff58c6716d3ace3
ning <ningyuan.sg@gmail.com>
2017-12-11 17:54:48 +0800

    Add 9b

--- day9_stream.py
+++ day9_stream.py
@@ -39,13 +39,17 @@ def count_score(stream, score_value):
                 to_remove.append(i)
                 to_remove.append(i+1)
                 counters['groups'] += 1
-            print(str(i) + ': ' + str(counters))
         for i in to_remove:
             stream = stream[:i] + 'x' + stream[i+1:]
-        print()
-        print(stream)
-        print(to_remove)
         return score_value * counters['groups'] \
             + count_score(re.sub('x', '', stream), score_value + 1)
 
 print(count_score(stream_sanitised, 1))  # Part 1: 16869
+
+def count_garbage(stream):
+    stream = remove_ignored(stream)
+    garbages = re.findall(garbage_re, stream)
+    return sum([ len(garbage)-2 for garbage in garbages ])
+
+print(count_garbage(stream))  # Part 2: 7284
+
